bb2987f36091c2db386d22ca6de1587259024fad
Asha Gaire <asha.gaire95@gmail.com>
2022-09-22 20:26:06 +0300

    first commit

---
+++ ModulesAndPackages/mod.py
@@ -0,0 +1,10 @@
+#Source:https://realpython.com/lessons/writing-module/
+
+s= 'Computers are useless. They can only give you answers.'
+a= [100, 200, 300]
+
+def printy(arg):
+    print(f'arg = {arg}')
+
+class Classy:
+    pass    
